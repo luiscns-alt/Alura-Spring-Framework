cb88cce014c880a1b1109426068c6fbffed6a26e
Luis Carlos <luiscarlosnors@gmail.com>
2022-11-08 11:15:55 -0300

    Assumindo o controle.

--- 02-DO_JDBC_AO_SPRING_DATA/01-Java e JDBC trabalhando com um banco de dados/loja-virtual-repository/src/InsercaoComParametro.java
+++ 02-DO_JDBC_AO_SPRING_DATA/01-Java e JDBC trabalhando com um banco de dados/loja-virtual-repository/src/InsercaoComParametro.java
@@ -2,15 +2,23 @@ import java.sql.*;
 
 public class InsercaoComParametro {
     public static void main(String[] args) throws SQLException {
-        String nome = "Teclado";
-        String descricao = "Multilaser";
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.RecuperarConexao();
+        connection.setAutoCommit(false);
 
         PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
 
+        AdicionarProduto("TV", "45pol", stm);
+        AdicionarProduto("Radio", "bateria", stm);
+    }
+
+    private static void AdicionarProduto(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
+
+        if (nome.equals("Radio")){
+            throw new RuntimeException("Não foi possível adicionar o produto");
+        }
 
         stm.execute();
 
@@ -20,6 +28,6 @@ public class InsercaoComParametro {
             Integer id = rst.getInt(1);
             System.out.println("ID criado: " + id);
         }
-        connection.close();
+        rst.close();
     }
 }
